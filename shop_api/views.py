e14d0c45790634a8414639d78ff94884b90312f2
hpaasch <hepaasch@gmail.com>
2016-06-30 15:52:13 -0400

    cat and subcat done. working on list within each.

--- shop_api/views.py
+++ shop_api/views.py
@@ -27,9 +27,23 @@ class SwapShopCategoryDetailAPIView(generics.RetrieveUpdateAPIView):
         return Category.objects.filter(choose_main=None)
 
 
-class SwapShopSubCategoryAPIView(generics.ListCreateAPIView):
+class SwapShopSubCatAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = SwapShopCategorySerializer
 
     def get_queryset(self):
-        return Category.objects.filter(choose_main=True)
+        return Category.objects.exclude(choose_main=None)  #
+
+
+class SwapShopSubCatDetailAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = SwapShopCategorySerializer
+
+
+class SwapShopCatListAPIView(generics.ListCreateAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = SwapShopListingSerializer
+
+    def get_queryset(self):
+        main_cat_id = self.kwargs.get('pk', None)
+        return Listing.objects.filter(pk=main_cat_id)
